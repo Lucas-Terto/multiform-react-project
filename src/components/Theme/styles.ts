bb7338d9c423a8caa40829105f73c8f395027a9f
Lucas-Terto <lucas47bk@gmail.com>
2022-08-05 20:32:00 -0300

    create theme

--- src/components/Theme/styles.ts
+++ src/components/Theme/styles.ts
@@ -1,9 +1,29 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-
+    background-color: #02044a;
+    color: #fff;
+    min-height: 100vh;
 `
 
 export const Area = styled.div`
-    
+    margin: auto;
+    max-width: 10em;
+    min-width: 100vh;
+    display: flex;
+    flex-direction: column;
+`
+
+export const Steps = styled.div`
+    flex: 1;
+    display: flex;
+`
+export const Sidebar = styled.div`
+    width: 2.5em;
+    border-right: 1px solid #16195c;
+`
+export const Page = styled.div`
+    flex: 1;
+    padding-left: 40px;
+    padding-top: 40px;
 `
