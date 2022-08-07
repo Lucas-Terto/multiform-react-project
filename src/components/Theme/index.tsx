ae2546126dbbac41ace2d8e6e757040f67f90073
Lucas-Terto <lucas47bk@gmail.com>
2022-08-07 11:59:44 -0300

    create sidebar

--- src/components/Theme/index.tsx
+++ src/components/Theme/index.tsx
@@ -1,12 +1,17 @@
 import * as C from './styles'
 import { ReactNode } from 'react';
 import {Header} from '../Header'
+import { SidebarItem } from './../SidebarItem/index';
+import {useForm} from '../../contexts/FormContext'
 
 type Props = {
     children:ReactNode
 }
 
 export const Theme = ({children}:Props) => {
+
+    const {state} = useForm()
+
     return (
         <C.Container>
             <C.Area>
@@ -14,7 +19,31 @@ export const Theme = ({children}:Props) => {
 
                 <C.Steps>
                     <C.Sidebar>
-                        ...
+
+                        <SidebarItem 
+                            title="Pessoal"
+                            description="Se identifique"
+                            icon="profile"
+                            path="/"
+                            active={state.currentStep === 1}
+                        />
+
+                        <SidebarItem 
+                            title="Profissional"
+                            description="Seu nível"
+                            icon="book"
+                            path="/step2"
+                            active={state.currentStep === 2}
+                        />
+
+                        <SidebarItem 
+                            title="Contatos"
+                            description="Como te achar"
+                            icon="mail"
+                            path="/step3"
+                            active={state.currentStep === 3}
+                        />
+                        
                     </C.Sidebar>
                     <C.Page>
                         {children}
